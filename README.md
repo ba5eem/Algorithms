b1a5740ccca2039f452e894aa2f806a34a29055e
ba5eem <baseem@gmail.com>
2018-03-14 11:36:51 -1000

    :smile:

--- README.md
+++ README.md
@@ -198,3 +198,30 @@ return reversedWordArr.join(' ');
 
 console.log(reverseWords("Let's take LeetCode contest"))
 ```
+
+## FizzBuzz - (easy):
+```js
+function fizzBuzz(n) {
+  let arr = [];
+  for(let i = 1; i < n+1; i++){
+    let fizz = i % 3 === 0;
+    let buzz = i % 5 === 0;
+    if(fizz & buzz){
+      arr.push('FizzBuzz');
+    }
+    else if(buzz){
+      arr.push('Buzz');
+    }
+    
+    else if(fizz){
+      arr.push('Fizz');
+    }
+    else if(!fizz && !buzz){
+      arr.push(i.toString());
+    }
+  }
+  return arr;   
+};
+
+console.log(fizzBuzz(15))
+```
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-function reverseWords(s) {
-    let reversedWordArr = [];
-    let x = s.split(' ');
-    for(let i = 0; i < x.length; i++){
-      let temp = [];
-      temp.push(x[i].split('').reverse().join(''))
-      reversedWordArr.push(temp[0])
-      temp.pop();
+function fizzBuzz(n) {
+  let arr = [];
+  for(let i = 1; i < n+1; i++){
+    let fizz = i % 3 === 0;
+    let buzz = i % 5 === 0;
+    if(fizz & buzz){
+      arr.push('FizzBuzz');
     }
-
-return reversedWordArr.join(' ');
+    else if(buzz){
+      arr.push('Buzz');
+    }
+    
+    else if(fizz){
+      arr.push('Fizz');
+    }
+    else if(!fizz && !buzz){
+      arr.push(i.toString());
+    }
+  }
+  return arr;   
 };
 
-console.log(reverseWords("Let's take LeetCode contest"))
+console.log(fizzBuzz(15))
