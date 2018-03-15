3595c8b0aef751ddfc3f4ee860d432a0cf6f73de
ba5eem <baseem@gmail.com>
2018-03-15 12:31:56 -1000

    update project docs

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-const hammingDistance = (x, y) => {
-  return arr = (x ^ y)
-    .toString(2)
-    .split('') //["1", "0", "1"]
-    .filter(elem => elem === "1").length; //2
-};
-console.log(hammingDistance(1,4));
 
 
 
 
 
-// function ddd(n) {
-//     let str = (n >>> 0).toString(2)
-//       //console.log(str); //1010
-//     let arr = str.split('');
-//       //console.log(arr); //[ '1', '0', '1', '0' ]
-//     let num = 0;
-//     for(var i = 0; i < arr.length; i++){
-//       if(arr[i] === '0'){
-//         num ++;
-//       }
-//     }
-//     return arr.length - num;
-// };
 
-// console.log(ddd(20));
+
+
+
+function findLongestWord(str) {
+  let arr = str.split(' ');
+  let n = 0;
+  for(let i = 0; i < arr.length; i++){
+    if(n < arr[i].length){
+      n = arr[i].length;
+    } 
+  }
+  return n;
+}
+let x = findLongestWord("The quick brown fox jumped over the lazy dog");
+
+console.log(x);
+
+
