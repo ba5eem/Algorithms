b14b0437ae800084d57e7d18cabfde40c437c7c6
ba5eem <baseem@gmail.com>
2018-03-15 12:24:58 -1000

    update project docs

--- index.js
+++ index.js
@@ -1,51 +1,31 @@
-function reverseBits(bits,n) {
-  let reversed = 0;
-  let last;
-  new Array(bits).fill(0).map(() => {
-    last = n & 1;
-    n >>= 1;
-    reversed <<=1;
-    reversed += last;
-  })
-  return reversed >>> 0;
+function hammingWeight(n) {
+  let arr = (n >>> 0)
+    .toString(2)
+    .split('')
+    return arr;
+
 }
 
-console.log(reverseBits(32, 964176192)); // 43261596
-console.log(reverseBits(32, 43261596)); // 964176192
+
+
+
+console.log(hammingWeight(10));
 
 
 
 
 
+function hammingWeight(n) {
+    let str = (n >>> 0).toString(2)
+      console.log(str); //1010
+    let arr = str.split('');
+      console.log(arr); //[ '1', '0', '1', '0' ]
+    let num = 0;
+    for(var i = 0; i < arr.length; i++){
+      if(arr[i] === '0'){
+        num ++;
+      }
+    }
+    return arr.length - num;
+};
 
-
-
-// function reverseBits(n) {
-//   let reversed = 0;
-//   let last;
-//   for(var i = 0; i < 32; i++){
-//     last = n & 1;
-//     n >>= 1;
-//     reversed <<=1;
-//     reversed += last
-//   }
-//   return reversed >>> 0;
-// };
-
-// console.log(reverseBits(43261596));
-
-
-
-// function fizzBuzz(n){
-//   let arr = [];
-//   new Array(n).fill(0).map((elem, i) => {
-//     return i+1;
-//   }).filter(elem => {
-//     (elem % 3 !== 0) && (elem % 5 !== 0) && arr.push(elem);
-//     (elem % 3 === 0) && (elem % 5 !== 0) && arr.push(elem + ': Fizz');
-//     (elem % 5 === 0) && (elem % 3 !== 0) && arr.push(elem + ': Buzz');
-//     (elem % 3 === 0) && (elem % 5 === 0) && arr.push(elem + ': FizzBuzz');
-//   })
-//   return arr;
-// }
-// console.log(fizzBuzz(15))
