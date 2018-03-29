59f9e2d95c0206c1740c1e9701ebcf9304221654
ba5eem <baseem@gmail.com>
2018-03-29 14:00:00 +0100

    changed function

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const findLongestWord = (str) => {
-  let arr = str.split(' ');
-  let n = 0;
-  for(let i = 0; i < arr.length; i++){
-    if(n < arr[i].length){
-      n = arr[i].length;
-    } 
-  }
-  return n;
-}
-findLongestWord("The quick brown fox jumped over the lazy dog");
+const hammingWeight = (n) => {
+    let str = (n >>> 0).toString(2)
+      console.log(str); //1010
+    let arr = str.split('');
+      console.log(arr); //[ '1', '0', '1', '0' ]
+    let num = 0;
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === '0'){
+        num ++;
+      }
+    }
+    return arr.length - num;
+};
+
+console.log(hammingWeight(10));
